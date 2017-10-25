99764bf5ff91faa81ae02d63a392496f5f3f85ef
Kim Nguyen <zktnguyen@ucdavis.edu>
2017-10-24 19:52:35 -0700

    CTCI changes

--- java/AnimalQueue.java
+++ java/AnimalQueue.java
@@ -3,5 +3,7 @@ public class AnimalQueue {
   // enqueue: if cat, add to cat Q, else add to dog Q
   // dequeue: if cat, remove from cat Q, else remove from dog Q
   // dequeueAny: marker flag of which one is in front of both queues.
-
+  public AnimalQueue() {
+    
+  }
 }
